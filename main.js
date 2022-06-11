da52029d5cf37960d75a99fac1d682b970f382df
Yasomalako <Yasomalako@gmail.com>
2022-06-11 13:08:58 +0300

    form

---
+++ main.js
@@ -0,0 +1,41 @@
+function calCultsubmit(){
+if(checkFirstname() && checkLastname() && checkBd() && checkEmil() && checkPhoneNumber() && checkPassword() && checkPassword() ){
+  return true;
+}
+return false;
+}
+
+function checkFirstname(){
+    if(first_Name[0] == first_Name[0].toUpperCase()){
+        true;
+    }
+    {
+        false; label_firstname.innerHTML="<span>plese start with capital leters </span>";
+        label_firstname.innerHTML.style.color ="red"
+    }
+}
+function cheackLastname(){
+    if([a-zA-Z]({10})){
+        
+    }
+}
+function cheackBd(){
+    if(user>18?){
+
+    }
+}
+function cheackEmail(){
+    if(){
+
+    }
+}
+function checkPhoneNumber(){
+    if(){
+
+    }
+}
+function checkPassword1(){
+    if(password1==password){
+        
+    }
+}
